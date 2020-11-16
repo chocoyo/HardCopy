087286dce553b811ad99d25ba4b6688840ea82ea
Mike Hodges <mikehodges7@outlook.com>
2020-11-15 21:52:20 -0600

    Added Movies To Active Admin

---
+++ app/admin/movies.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register Movie do
+  permit_params :release_date, :rating, :description, :director, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions
+  end
+end
