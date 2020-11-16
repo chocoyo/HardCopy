d7294f60137ea7db1cb067f4563a518995b210ec
Mike Hodges <mikehodges7@outlook.com>
2020-11-16 15:37:46 -0600

    Added Basic Seed Script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Setting up active admin"
 AdminUser.destroy_all
 
 if Rails.env.development?
   AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
 end
+
+puts "Deleting data"
+Movie.destroy_all
+Genre.destroy_all
+
+puts "Adding basic data"
+action = Genre.create(title: "Action")
+horror = Genre.create(title: "Horror")
+family = Genre.create(title: "Family")
+
+Movie.create(title: "Mission Impossible", director: "Not Me", description: "Crazy Movie", genre: action)
+Movie.create(title: "Very Scary Movie", director: "Also Not Me", description: "Crazy Movie", genre: horror)
+Movie.create(title: "Cars", director: "Lighting McQueen", description: "Crazy Movie", genre: family)
+
+puts "Added Data"
