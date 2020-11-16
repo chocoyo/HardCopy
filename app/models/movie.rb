a09e65c36138f0a1c8564db521d0783d1f3aae1e
Mike Hodges <mikehodges7@outlook.com>
2020-11-15 22:29:04 -0600

    Added Reference From Movies To Genre

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
+  has_one :genres
   has_one_attached :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_042232) do
+ActiveRecord::Schema.define(version: 2020_11_16_042613) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -73,7 +73,10 @@ ActiveRecord::Schema.define(version: 2020_11_16_042232) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.integer "genre_id", null: false
+    t.index ["genre_id"], name: "index_movies_on_genre_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "movies", "genres"
 end
