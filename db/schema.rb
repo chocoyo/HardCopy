733abccf5879bfb86c53a4e8d87edb38da1f2a9f
Mike Hodges <mikehodges7@outlook.com>
2020-11-23 16:09:34 -0600

    Added Basic Fields To Account Registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_213556) do
+ActiveRecord::Schema.define(version: 2020_11_23_214626) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -97,6 +97,10 @@ ActiveRecord::Schema.define(version: 2020_11_18_213556) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
