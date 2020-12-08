895a0467093bb5f3359b0f94ea1d74911a4f0936
Mike Hodges <mikehodges7@outlook.com>
2020-12-07 22:08:44 -0600

    Added Price To Movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_215018) do
+ActiveRecord::Schema.define(version: 2020_12_08_040517) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_215018) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.integer "genre_id", null: false
+    t.integer "price"
     t.index ["genre_id"], name: "index_movies_on_genre_id"
   end
 
